6e41d8e025a82a369ba243cf562a452e3c27c412
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-03-03 18:04:23 +0900

    update

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,3 @@
-document.addEventListener('load', () => {
-	Array.from(document.querySelectorAll(".u-list.has-linked-children li.movie")).forEach(ele => {
-		ele.style.display = 'none';
-	})
-})
+Array.from(document.querySelectorAll(".u-list.has-linked-children li.movie")).forEach(ele => {
+	ele.style.display = 'none';
+});
